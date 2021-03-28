2f3477b2137e3e39ebf964388a67a7705b8c49c2
Andrew Clark <ac8774@gmail.com>
2021-03-28 01:34:24 -0500

    more words

--- words.js
+++ words.js
@@ -2,12 +2,19 @@ var wordlist = `
 
 Huntsville
 Alabama
+rocket city
+trash pandas
+Space Camp
+Saturn V
+Wernher von Braun
+Redstone
 
 University of Alabama
 university
 college
 engineering
 engineer
+go Chargers
 
 space
 rocket
@@ -19,9 +26,14 @@ sun
 black hole
 comet
 Earth
+Mars
 star
 interstellar
-Milky way
+Milky Way
 NASA
+space race
+Apollo
+propulsion
+takeoff
 
 `
